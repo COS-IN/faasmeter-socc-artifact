db2761f02d81d2fe026b1ff6eecc622fb09d97f6
Alex Fuerst <alfuerst@iu.edu>
2024-10-07 18:18:36 -0400

    ./trace/fig_9/desktop//mc_4f_traces_ddm_15min/ data

---
+++ results/trace/fig_9/desktop/mc_4f_traces_ddm_15min/analyze.sh
@@ -0,0 +1,11 @@
+python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                -s mc_*/functions/fcfs/12/12/    \
+                                -m desktop    \
+                                -o indiv    \
+                                -q quirk_trim_end_by_time    \
+                                -d 2.1 &&    \
+python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                -s mc_*/functions/fcfs/12/12/    \
+                                -m desktop    \
+                                -o indiv    \
+                                -p
