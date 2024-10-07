67085e53aaf9544fc61e66c200da8e3322184b33
Alex Fuerst <alfuerst@iu.edu>
2024-10-07 18:21:32 -0400

    ./trace/for_paper/victor//nddp/ data

---
+++ results/trace/for_paper/victor/nddp/mc_4f_traces_nddp_15min/analyze_sever_multi_lg.sh
@@ -0,0 +1,16 @@
+python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                -s mc_*/functions/fcfs/24/24/    \
+                                -m server    \
+                                -o indiv    \
+                                -q quirk_trim_end_by_time    \
+                                --plots_only  \
+                                -d 2.1 &&    \
+python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                -s mc_*/functions/fcfs/24/24/    \
+                                -m server    \
+                                -o indiv    \
+                                --plots_only  
+                                
+
+                                #--no_singular  \
+                                #--no_parsing  \
---
+++ results/trace/for_paper/victor/nddp/scaphandre/mc_4f_traces_nddp_15min/analyze_sever_single_lg.sh
@@ -0,0 +1,15 @@
+python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                -l ./mc_a/functions/fcfs/24/24/    \
+                                -m server    \
+                                -o indiv    \
+                                -q quirk_trim_end_by_time    \
+                                -d 2.1 &&    \
+python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                -l ./mc_a/functions/fcfs/24/24/    \
+                                -m server    \
+                                -m server    \
+                                -o indiv    \
+                                -p
+
+
+                                # -s mc_*/functions/fcfs/12/12/    \
