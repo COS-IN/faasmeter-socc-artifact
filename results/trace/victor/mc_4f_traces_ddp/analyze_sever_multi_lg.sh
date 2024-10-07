b0b7a8eccd5cff93f2b547ba7e0c501c88a9f199
Alex Fuerst <alfuerst@iu.edu>
2024-10-07 18:09:07 -0400

    ./trace/victor/mc_4f_traces_ddp/ data

---
+++ results/trace/victor/mc_4f_traces_ddp/analyze_sever_multi_lg.sh
@@ -0,0 +1,17 @@
+python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                -s mc_*/functions/fcfs/24/24/    \
+                                -m server    \
+                                -o indiv    \
+                                -q quirk_trim_end_by_time    \
+                                --no_singular \
+                                --no_parsing \
+                                -d 2.1 &&    \
+python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                -s mc_*/functions/fcfs/24/24/    \
+                                -m server    \
+                                --no_singular \
+                                --no_parsing \
+                                -o indiv    \
+                                -p
+
+
