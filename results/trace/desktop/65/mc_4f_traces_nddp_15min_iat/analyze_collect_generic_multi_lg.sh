7d331fe5c32fbd2272a2dc73e70da5b22cb2e43f
Alex Fuerst <alfuerst@iu.edu>
2024-10-07 18:22:20 -0400

    ./trace/desktop/65//mc_4f_traces_nddp_15min_iat/ data

---
+++ results/trace/desktop/65/mc_4f_traces_nddp_15min_iat/analyze_collect_generic_multi_lg.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+IFS=/ read -a wds < <(pwd)
+for w in "${wds[@]}"; do 
+  if [[ "$w" == "victor" ]]; then 
+    platform=server
+    cores=24
+    break
+  elif [[ "$w" == "desktop" ]]; then
+    platform=desktop
+    cores=12
+    break
+  fi
+done
+
+# everything 
+if true; then 
+  python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                  -l mc_*/functions/fcfs/$cores/$cores/    \
+                                  -m "$platform"    \
+                                  -o indiv    \
+                                  -q quirk_trim_end_by_time    \
+                                  -d 2.1 &&    \
+  python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                  -l mc_*/functions/fcfs/$cores/$cores/    \
+                                  -m "$platform"    \
+                                  -o indiv    \
+                                  -p
+
+# no analysis just collection and plotting 
+else
+  python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                  -l ./scaphandre/mc_4f_traces_nddp_15min/mc_a/functions/fcfs/$cores/$cores/    \
+                                  -m "$platform"    \
+                                  -o indiv    \
+                                  -q quirk_trim_end_by_time    \
+                                  --no_singular  \
+                                  --no_parsing  \
+                                  -d 2.1 &&    \
+  python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                  -s mc_*/functions/fcfs/$cores/$cores/    \
+                                  -m "$platform"    \
+                                  -o indiv    \
+                                  -q quirk_trim_end_by_time    \
+                                  --no_singular  \
+                                  --no_parsing  \
+                                  -d 2.1 &&    \
+  python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                  --lg_log_dirs \
+                                      ./mc_*/functions/fcfs/$cores/$cores/    \
+                                      ./scaphandre/mc_4f_traces_nddp_15min/mc_*/functions/fcfs/$cores/$cores/    \
+                                  --mc_type "$platform" \
+                                  --collection_only && \
+  python3 /data2/ar/faasmeter/faasmeter/main.py    \
+                                  -s mc_*/functions/fcfs/$cores/$cores/    \
+                                  -m "$platform"    \
+                                  -o indiv    \
+                                  --plots_only
+fi
+
+                                #--plots_only  \
+                                #--no_singular  \
+                                #--no_parsing  \
+
